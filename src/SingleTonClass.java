e567f9b68b115abf1fc8c58e91f4cb2820e417a7
TotalEnnui <aprpkp@gmail.com>
2019-05-17 18:31:50 -0400

    Experimental 1.0

--- src/SingleTonClass.java
+++ src/SingleTonClass.java
@@ -1,27 +1,41 @@
 public class SingleTonClass {
-   //Static Class Reference
-   private static SingleTonClass obj=null;
-   private SingleTonClass(){
-      /*Private Constructor will prevent 
-       * the instantiation of this class directly*/
-   }
-   public static SingleTonClass objectCreationMethod(){
-	/*This logic will ensure that no more than
-	 * one object can be created at a time */
-	if(obj==null){
-	    obj= new SingleTonClass();
-	}
-        return obj;
-   }
-   public void display(){
-	System.out.println("Singleton class Example");
-   }
-   public static void main(String args[]){
-	//Object cannot be created directly due to private constructor 
-        //This way it is forced to create object via our method where
-        //we have logic for only one object creation
-	SingleTonClass myobject= SingleTonClass.objectCreationMethod();
-	myobject.display();
-   }
-}
+	// Static Class Reference
 
+	private static int objCount = 0;
+	private static SingleTonClass obj = null;
+
+	private SingleTonClass() {
+		/*
+		 * Private Constructor will prevent the instantiation of this class directly
+		 */
+	}
+
+	public static SingleTonClass objectCreationMethod() {
+
+		if (objCount < 3) {
+			obj = new SingleTonClass();
+			objCount++;
+			System.out.println(objCount);
+		}
+		return obj;
+	}
+
+	public void display() {
+		System.out.println("Singleton class Example " + objCount);
+	}
+
+	public static void main(String args[]) {
+		// Object cannot be created directly due to private constructor
+		// This way it is forced to create object via our method where
+		// we have logic for only one object creation
+		SingleTonClass myobject = SingleTonClass.objectCreationMethod();
+		myobject.display();
+		SingleTonClass myobject2 = SingleTonClass.objectCreationMethod();
+		myobject2.display();
+		SingleTonClass myobject3 = SingleTonClass.objectCreationMethod();
+		myobject3.display();
+		SingleTonClass myobject4 = SingleTonClass.objectCreationMethod();
+		myobject4.display();
+		
+	}
+}
